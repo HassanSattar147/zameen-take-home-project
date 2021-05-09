e008501511124666c919c8de570d12df0e1c043e
hassansattar147 <hassansattar147@gmail.com>
2021-05-09 17:06:22 +0500

    Working app. But not beautiful.

--- src/App.js
+++ src/App.js
@@ -6,123 +6,13 @@ import CurrentUserGistsContainer from "./components/CurrentUserGistsContainer/Cu
 
 export default class App extends Component {
   state = {
-    currentUser: {
-      currUserAvatarUrl: "https://avatars.githubusercontent.com/u/483012?v=4",
-      currUserName: "Yannick Schutz",
-      currUserLogin: "ys",
-    },
-    currentUserGists: [
-      {
-        files: [
-          {
-            filename: "heroku_build.rb",
-            type: "application/x-ruby",
-            language: "Ruby",
-            content:
-              '    def tar(path)\n      base_path = path.split("/")[0..-2].join("/")\n      folder = path.split("/")[-1]\n      if File.exist?(".gitignore")\n        ok = system("/usr/bin/tar -C \\"#{base_path}\\" --exclude=\'.git\' --exclude-from=.gitignore -cvzf \\"#{tempfile}\\" \\"#{folder}\\"")\n      else\n        ok = system("/usr/bin/tar -C \\"#{base_path}\\" -cvzf \\"#{tempfile}\\" \\"#{folder}\\"")\n      end\n      unless ok\n        raise StandardError, $?\n      end\n      tempfile\n    end\n',
-          },
-          {
-            filename: "heroku.py",
-            type: "code/python",
-            language: "Python",
-            content:
-              '    def tar(path)\n      base_path = path.split("/")[0..-2].join("/")\n      folder = path.split("/")[-1]\n      if File.exist?(".gitignore")\n        ok = system("/usr/bin/tar -C \\"#{base_path}\\" --exclude=\'.git\' --exclude-from=.gitignore -cvzf \\"#{tempfile}\\" \\"#{folder}\\"")\n      else\n        ok = system("/usr/bin/tar -C \\"#{base_path}\\" -cvzf \\"#{tempfile}\\" \\"#{folder}\\"")\n      end\n      unless ok\n        raise StandardError, $?\n      end\n      tempfile\n    end\n',
-          },
-        ],
-        forks: [
-          {
-            login: "fork1login",
-            name: "Fork 1 Login",
-            avatar_url: "https://avatars.githubusercontent.com/u/483012?v=4",
-          },
-          {
-            login: "fork2login",
-            name: "Fork 2 Login",
-            avatar_url: "https://avatars.githubusercontent.com/u/483012?v=4",
-          },
-          {
-            login: "fork3login",
-            name: "Fork 3 Login",
-            avatar_url: "https://avatars.githubusercontent.com/u/483012?v=4",
-          },
-        ],
-      },
-      {
-        files: [
-          {
-            filename: "heroku_build.rb",
-            type: "application/x-ruby",
-            language: "Ruby",
-            content:
-              '    def tar(path)\n      base_path = path.split("/")[0..-2].join("/")\n      folder = path.split("/")[-1]\n      if File.exist?(".gitignore")\n        ok = system("/usr/bin/tar -C \\"#{base_path}\\" --exclude=\'.git\' --exclude-from=.gitignore -cvzf \\"#{tempfile}\\" \\"#{folder}\\"")\n      else\n        ok = system("/usr/bin/tar -C \\"#{base_path}\\" -cvzf \\"#{tempfile}\\" \\"#{folder}\\"")\n      end\n      unless ok\n        raise StandardError, $?\n      end\n      tempfile\n    end\n',
-          },
-          {
-            filename: "heroku.py",
-            type: "code/python",
-            language: "Python",
-            content:
-              '    def tar(path)\n      base_path = path.split("/")[0..-2].join("/")\n      folder = path.split("/")[-1]\n      if File.exist?(".gitignore")\n        ok = system("/usr/bin/tar -C \\"#{base_path}\\" --exclude=\'.git\' --exclude-from=.gitignore -cvzf \\"#{tempfile}\\" \\"#{folder}\\"")\n      else\n        ok = system("/usr/bin/tar -C \\"#{base_path}\\" -cvzf \\"#{tempfile}\\" \\"#{folder}\\"")\n      end\n      unless ok\n        raise StandardError, $?\n      end\n      tempfile\n    end\n',
-          },
-        ],
-        forks: [
-          {
-            login: "fork1login",
-            name: "Fork 1 Login",
-            avatar_url: "https://avatars.githubusercontent.com/u/483012?v=4",
-          },
-          {
-            login: "fork2login",
-            name: "Fork 2 Login",
-            avatar_url: "https://avatars.githubusercontent.com/u/483012?v=4",
-          },
-          {
-            login: "fork3login",
-            name: "Fork 3 Login",
-            avatar_url: "https://avatars.githubusercontent.com/u/483012?v=4",
-          },
-        ],
-      },
-      {
-        files: [
-          {
-            filename: "heroku_build.rb",
-            type: "application/x-ruby",
-            language: "Ruby",
-            content:
-              '    def tar(path)\n      base_path = path.split("/")[0..-2].join("/")\n      folder = path.split("/")[-1]\n      if File.exist?(".gitignore")\n        ok = system("/usr/bin/tar -C \\"#{base_path}\\" --exclude=\'.git\' --exclude-from=.gitignore -cvzf \\"#{tempfile}\\" \\"#{folder}\\"")\n      else\n        ok = system("/usr/bin/tar -C \\"#{base_path}\\" -cvzf \\"#{tempfile}\\" \\"#{folder}\\"")\n      end\n      unless ok\n        raise StandardError, $?\n      end\n      tempfile\n    end\n',
-          },
-          {
-            filename: "heroku.py",
-            type: "code/python",
-            language: "Python",
-            content:
-              '    def tar(path)\n      base_path = path.split("/")[0..-2].join("/")\n      folder = path.split("/")[-1]\n      if File.exist?(".gitignore")\n        ok = system("/usr/bin/tar -C \\"#{base_path}\\" --exclude=\'.git\' --exclude-from=.gitignore -cvzf \\"#{tempfile}\\" \\"#{folder}\\"")\n      else\n        ok = system("/usr/bin/tar -C \\"#{base_path}\\" -cvzf \\"#{tempfile}\\" \\"#{folder}\\"")\n      end\n      unless ok\n        raise StandardError, $?\n      end\n      tempfile\n    end\n',
-          },
-        ],
-        forks: [
-          {
-            login: "fork1login",
-            name: "Fork 1 Login",
-            avatar_url: "https://avatars.githubusercontent.com/u/483012?v=4",
-          },
-          {
-            login: "fork2login",
-            name: "Fork 2 Login",
-            avatar_url: "https://avatars.githubusercontent.com/u/483012?v=4",
-          },
-          {
-            login: "fork3login",
-            name: "Fork 3 Login",
-            avatar_url: "https://avatars.githubusercontent.com/u/483012?v=4",
-          },
-        ],
-      },
-    ],
+    currentUser: {},
+    currentUserGists: [],
     searchField: "",
   };
 
   searchUser = (str) => {
-    // this.getAndSetData(str);
+    this.getAndSetData(str);
     console.log(`${str} from SearchBox`);
   };
 
@@ -131,8 +21,8 @@ export default class App extends Component {
       <div>
         <NavBar />
         <SearchBox handleSearchUser={this.searchUser} />
-        <CurrentUserCard currentUserDetails ={this.state.currentUser} />
-        <CurrentUserGistsContainer gistsDetails= {this.state.currentUserGists} />
+        <CurrentUserCard currentUserDetails={this.state.currentUser} />
+        <CurrentUserGistsContainer gistsDetails={this.state.currentUserGists} />
       </div>
     );
   }
@@ -140,8 +30,8 @@ export default class App extends Component {
   // helper functions
 
   componentDidMount() {
-    // let searchUrl = this.state.searchField || "ys";
-    // this.getAndSetData(searchUrl);
+    let searchUrl = this.state.searchField || "ys";
+    this.getAndSetData(searchUrl);
   }
 
   getAndSetData = async (user) => {
@@ -213,3 +103,120 @@ export default class App extends Component {
     console.log("All Gists", _currentUserGists);
   };
 }
+
+// Test state
+// state = {
+//   currentUser: {
+//     currUserAvatarUrl: "https://avatars.githubusercontent.com/u/483012?v=4",
+//     currUserName: "Yannick Schutz",
+//     currUserLogin: "ys",
+//   },
+//   currentUserGists: [
+//     {
+//       files: [
+//         {
+//           filename: "heroku_build.rb",
+//           type: "application/x-ruby",
+//           language: "Ruby",
+//           content:
+//             '    def tar(path)\n      base_path = path.split("/")[0..-2].join("/")\n      folder = path.split("/")[-1]\n      if File.exist?(".gitignore")\n        ok = system("/usr/bin/tar -C \\"#{base_path}\\" --exclude=\'.git\' --exclude-from=.gitignore -cvzf \\"#{tempfile}\\" \\"#{folder}\\"")\n      else\n        ok = system("/usr/bin/tar -C \\"#{base_path}\\" -cvzf \\"#{tempfile}\\" \\"#{folder}\\"")\n      end\n      unless ok\n        raise StandardError, $?\n      end\n      tempfile\n    end\n',
+//         },
+//         {
+//           filename: "heroku.py",
+//           type: "code/python",
+//           language: "Python",
+//           content:
+//             '    def tar(path)\n      base_path = path.split("/")[0..-2].join("/")\n      folder = path.split("/")[-1]\n      if File.exist?(".gitignore")\n        ok = system("/usr/bin/tar -C \\"#{base_path}\\" --exclude=\'.git\' --exclude-from=.gitignore -cvzf \\"#{tempfile}\\" \\"#{folder}\\"")\n      else\n        ok = system("/usr/bin/tar -C \\"#{base_path}\\" -cvzf \\"#{tempfile}\\" \\"#{folder}\\"")\n      end\n      unless ok\n        raise StandardError, $?\n      end\n      tempfile\n    end\n',
+//         },
+//       ],
+//       forks: [
+//         {
+//           login: "fork1login",
+//           name: "Fork 1 Login",
+//           avatar_url: "https://avatars.githubusercontent.com/u/483012?v=4",
+//         },
+//         {
+//           login: "fork2login",
+//           name: "Fork 2 Login",
+//           avatar_url: "https://avatars.githubusercontent.com/u/483012?v=4",
+//         },
+//         {
+//           login: "fork3login",
+//           name: "Fork 3 Login",
+//           avatar_url: "https://avatars.githubusercontent.com/u/483012?v=4",
+//         },
+//       ],
+//     },
+//     {
+//       files: [
+//         {
+//           filename: "heroku_build.rb",
+//           type: "application/x-ruby",
+//           language: "Ruby",
+//           content:
+//             '    def tar(path)\n      base_path = path.split("/")[0..-2].join("/")\n      folder = path.split("/")[-1]\n      if File.exist?(".gitignore")\n        ok = system("/usr/bin/tar -C \\"#{base_path}\\" --exclude=\'.git\' --exclude-from=.gitignore -cvzf \\"#{tempfile}\\" \\"#{folder}\\"")\n      else\n        ok = system("/usr/bin/tar -C \\"#{base_path}\\" -cvzf \\"#{tempfile}\\" \\"#{folder}\\"")\n      end\n      unless ok\n        raise StandardError, $?\n      end\n      tempfile\n    end\n',
+//         },
+//         {
+//           filename: "heroku.py",
+//           type: "code/python",
+//           language: "Python",
+//           content:
+//             '    def tar(path)\n      base_path = path.split("/")[0..-2].join("/")\n      folder = path.split("/")[-1]\n      if File.exist?(".gitignore")\n        ok = system("/usr/bin/tar -C \\"#{base_path}\\" --exclude=\'.git\' --exclude-from=.gitignore -cvzf \\"#{tempfile}\\" \\"#{folder}\\"")\n      else\n        ok = system("/usr/bin/tar -C \\"#{base_path}\\" -cvzf \\"#{tempfile}\\" \\"#{folder}\\"")\n      end\n      unless ok\n        raise StandardError, $?\n      end\n      tempfile\n    end\n',
+//         },
+//       ],
+//       forks: [
+//         {
+//           login: "fork1login",
+//           name: "Fork 1 Login",
+//           avatar_url: "https://avatars.githubusercontent.com/u/483012?v=4",
+//         },
+//         {
+//           login: "fork2login",
+//           name: "Fork 2 Login",
+//           avatar_url: "https://avatars.githubusercontent.com/u/483012?v=4",
+//         },
+//         {
+//           login: "fork3login",
+//           name: "Fork 3 Login",
+//           avatar_url: "https://avatars.githubusercontent.com/u/483012?v=4",
+//         },
+//       ],
+//     },
+//     {
+//       files: [
+//         {
+//           filename: "heroku_build.rb",
+//           type: "application/x-ruby",
+//           language: "Ruby",
+//           content:
+//             '    def tar(path)\n      base_path = path.split("/")[0..-2].join("/")\n      folder = path.split("/")[-1]\n      if File.exist?(".gitignore")\n        ok = system("/usr/bin/tar -C \\"#{base_path}\\" --exclude=\'.git\' --exclude-from=.gitignore -cvzf \\"#{tempfile}\\" \\"#{folder}\\"")\n      else\n        ok = system("/usr/bin/tar -C \\"#{base_path}\\" -cvzf \\"#{tempfile}\\" \\"#{folder}\\"")\n      end\n      unless ok\n        raise StandardError, $?\n      end\n      tempfile\n    end\n',
+//         },
+//         {
+//           filename: "heroku.py",
+//           type: "code/python",
+//           language: "Python",
+//           content:
+//             '    def tar(path)\n      base_path = path.split("/")[0..-2].join("/")\n      folder = path.split("/")[-1]\n      if File.exist?(".gitignore")\n        ok = system("/usr/bin/tar -C \\"#{base_path}\\" --exclude=\'.git\' --exclude-from=.gitignore -cvzf \\"#{tempfile}\\" \\"#{folder}\\"")\n      else\n        ok = system("/usr/bin/tar -C \\"#{base_path}\\" -cvzf \\"#{tempfile}\\" \\"#{folder}\\"")\n      end\n      unless ok\n        raise StandardError, $?\n      end\n      tempfile\n    end\n',
+//         },
+//       ],
+//       forks: [
+//         {
+//           login: "fork1login",
+//           name: "Fork 1 Login",
+//           avatar_url: "https://avatars.githubusercontent.com/u/483012?v=4",
+//         },
+//         {
+//           login: "fork2login",
+//           name: "Fork 2 Login",
+//           avatar_url: "https://avatars.githubusercontent.com/u/483012?v=4",
+//         },
+//         {
+//           login: "fork3login",
+//           name: "Fork 3 Login",
+//           avatar_url: "https://avatars.githubusercontent.com/u/483012?v=4",
+//         },
+//       ],
+//     },
+//   ],
+//   searchField: "",
+// };
--- src/components/FilesCard/FileCard.component.jsx
+++ src/components/FilesCard/FileCard.component.jsx
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./file-card.styles.css";
 
 function FileCard({ fileDetails }) {
   const { filename, type, language, content } = fileDetails;
+  const [isContentVisible, setIsContentVisible] = useState(false);
   return (
     <div>
       <div className="file-card">
         <p>{filename}</p>
         <p>{type}</p>
         <p>{language}</p>
-        <p>{content}</p>
+        <button onClick={() => setIsContentVisible(!isContentVisible)}>
+          {isContentVisible ? "Hide " : "Show "} Content
+        </button>
+        {isContentVisible && <p>{content}</p>}
       </div>
     </div>
   );
